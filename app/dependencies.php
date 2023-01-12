c03b4f7f0893ffb9e7cdeee4292fa9a9a08b7137
Mikey Ying <mikeyinguk@gmail.com>
2023-01-12 09:48:37 +0000

    refactored db connection to use a PDOFactory and ::class notation

--- app/dependencies.php
+++ app/dependencies.php
@@ -12,16 +12,24 @@ use Slim\Views\PhpRenderer;
 
 return function (ContainerBuilder $containerBuilder) {
     $container = [];
+    // Global Settings Object
+    $containerBuilder->addDefinitions([
+        'settings' => [
+            'displayErrorDetails' => true, // should be set to false in production
+            'db' => [
+                'host' => 'mysql:host=DB;',
+                'name' => 'dbname=tech_skills',
+                'user' => 'root',
+                'password' => 'password'
+            ]
+        ]
+    ]);
 
     $container[LoggerInterface::class] = DI\factory(LoggerFactory::class);
     $container[PhpRenderer::class] = DI\factory(RendererFactory::class);
     
     // db connection
-    $container['db'] = function () {
-        $db = new \PDO('mysql:host=DB; dbname=tech_skills', 'root', 'password');
-        $db->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
-        return $db;
-    };
+    $container[db::class] = DI\factory(PDOFactory::class);
 
     // factories
     $container['TopicsModel'] = DI\Factory(TopicsModelFactory::class);
