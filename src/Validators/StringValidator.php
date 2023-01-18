090ab6db9ed840f22ab45d6892519e79a56ec870
Mikey Ying <mikeyinguk@gmail.com>
2023-01-18 15:02:24 +0000

    created string validator

---
+++ src/Validators/StringValidator.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Validators;
+
+use App\CustomExceptions\InvalidTopicException;
+
+class StringValidator
+{
+    /**
+     * Checks if a string is empty and whether it is <= a given length
+     *
+     * @param string $validateData
+     * @param integer $length
+     * @return bool
+     */
+    public static function validateExistsAndLength(string $validateData, int $length): bool
+    {
+        if (!empty($validateData) && strlen($validateData) <= $length) {
+            return true;
+        }
+
+        return false;
+    }
+}
