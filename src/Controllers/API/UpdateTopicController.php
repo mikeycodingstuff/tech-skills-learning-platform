94169164fe28209d21a23283bdf17c6a559ec200
Mikey Ying <mikeyinguk@gmail.com>
2023-01-18 15:57:40 +0000

    added validation to update controller

--- src/Controllers/API/UpdateTopicController.php
+++ src/Controllers/API/UpdateTopicController.php
@@ -3,9 +3,11 @@
 namespace App\Controllers\API;
 
 use App\CustomExceptions\InvalidIdException;
+use App\CustomExceptions\InvalidTopicException;
 use App\CustomExceptions\MissingTopicException;
 use App\Models\TopicModel;
 use App\Sanitisers\TopicSanitiser;
+use App\Validators\TopicValidator;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
@@ -31,11 +33,16 @@ class UpdateTopicController
         $updatedTopic['id'] = $args['id'];
 
         try {
-            $updatedTopic = TopicSanitiser::sanitiseTopic($updatedTopic);
-            $this->topicModel->updateTopicById($updatedTopic);
-            $responseBody['data'] = $this->topicModel->getTopicById($updatedTopic['id']);
-            $responseBody['success'] = true;
-            $responseBody['message'] = 'Topic successfully updated in database.';
+            if (TopicValidator::validateTopic($updatedTopic)) {
+                $updatedTopic = TopicSanitiser::sanitiseTopic($updatedTopic);
+                $this->topicModel->updateTopicById($updatedTopic);
+                $responseBody['data'] = $this->topicModel->getTopicById($updatedTopic['id']);
+                $responseBody['success'] = true;
+                $responseBody['message'] = 'Topic successfully updated in database.';
+            }
+        } catch (InvalidTopicException $e) {
+            $responseBody['message'] = $e->getMessage();
+            $responseBody['status'] = 400;
         } catch (InvalidIdException $e) {
             $responseBody['message'] = $e->getMessage();
             $responseBody['status'] = 404;
