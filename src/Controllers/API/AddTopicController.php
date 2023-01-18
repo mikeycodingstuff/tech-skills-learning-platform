17ab52e12f3c53e512efef42a55a81078ffdb4c0
Mikey Ying <mikeyinguk@gmail.com>
2023-01-18 15:32:34 +0000

    incorporated validation to add controller

--- src/Controllers/API/AddTopicController.php
+++ src/Controllers/API/AddTopicController.php
@@ -3,9 +3,11 @@
 namespace App\Controllers\API;
 
 use App\CustomExceptions\InvalidIdException;
+use App\CustomExceptions\InvalidTopicException;
 use App\CustomExceptions\MissingTopicException;
 use App\Models\TopicModel;
 use App\Sanitisers\TopicSanitiser;
+use App\Validators\TopicValidator;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
@@ -28,14 +30,19 @@ class AddTopicController
             'status' => 200,
             'data' => []
         ];
-
+        
         try {
-            $newTopic = TopicSanitiser::sanitiseTopic($newTopic);
-            $this->topicModel->addTopic($newTopic);
-            $newTopicId = $this->topicModel->getLastTopicId();
-            $responseBody['success'] = true;
-            $responseBody['message'] = 'Topic successfully added to db.';
-            $responseBody['data'] = $this->topicModel->getTopicById($newTopicId);
+            if (TopicValidator::validateTopic($newTopic)) {
+                $newTopic = TopicSanitiser::sanitiseTopic($newTopic);
+                $this->topicModel->addTopic($newTopic);
+                $newTopicId = $this->topicModel->getLastTopicId();
+                $responseBody['success'] = true;
+                $responseBody['message'] = 'Topic successfully added to db.';
+                $responseBody['data'] = $this->topicModel->getTopicById($newTopicId);
+            }
+        } catch (InvalidTopicException $e) {
+            $responseBody['message'] = $e->getMessage();
+            $responseBody['status'] = 400;
         } catch (InvalidIdException $e) {
             $responseBody['message'] = $e->getMessage();
             $responseBody['status'] = 404;
